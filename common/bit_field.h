sonburton99/shader-compiler

Implement yuzu `common` within license exemption constraints
The shader compiler use a subset of yuzu's `common`, this contains a lot of GPL code that isn't covered under our exemptions so we needed to selectively reimplement parts of it alongside certain parts that just don't map to Skyline such as `Settings`.

common/bit_field.h
@@ -1,0 +1,40 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright © 2019 yuzu Emulator Project (https://github.com/yuzu-emu/yuzu/)
+
+#pragma once
+
+#include "common_types.h"
+#include "bit_cast.h"
+
+namespace Shader {
+    #pragma pack(push, 1)
+    template<size_t Start, size_t Count, typename Type>
+    struct BitField {
+      private:
+        constexpr static size_t BitsInByte{8};
+        constexpr static size_t PaddingBits{Start % BitsInByte};
+
+        u8 paddingBytes[Start / BitsInByte];
+        size_t item : PaddingBits + Count;
+
+      public:
+        constexpr void Assign(const Type &value) {
+            constexpr size_t PaddingMask{(1 << PaddingBits) - 1};
+            size_t newValue{static_cast<size_t>(value) << PaddingBits};
+            item = newValue | (item & PaddingMask);
+        }
+
+        constexpr Type Value() const {
+            return static_cast<Type>(item >> PaddingBits);
+        }
+
+        constexpr operator Type() const {
+            return Value();
+        }
+
+        constexpr explicit operator bool() const {
+            return Value() != 0;
+        }
+    };
+    #pragma pack(pop)
+}

skyline-emu/audio-core

common/bit_field.h
@@ -1,0 +1,40 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright © 2019 yuzu Emulator Project (https://github.com/yuzu-emu/yuzu/)
+
+#pragma once
+
+#include "common_types.h"
+#include "bit_cast.h"
+
+namespace AudioCore {
+    #pragma pack(push, 1)
+    template<size_t Start, size_t Count, typename Type>
+    struct BitField {
+      private:
+        constexpr static size_t BitsInByte{8};
+        constexpr static size_t PaddingBits{Start % BitsInByte};
+
+        u8 paddingBytes[Start / BitsInByte];
+        size_t item : PaddingBits + Count;
+
+      public:
+        constexpr void Assign(const Type &value) {
+            constexpr size_t PaddingMask{(1 << PaddingBits) - 1};
+            size_t newValue{static_cast<size_t>(value) << PaddingBits};
+            item = newValue | (item & PaddingMask);
+        }
+
+        constexpr Type Value() const {
+            return static_cast<Type>(item >> PaddingBits);
+        }
+
+        constexpr operator Type() const {
+            return Value();
+        }
+
+        constexpr explicit operator bool() const {
+            return Value() != 0;
+        }
+    };
+    #pragma pack(pop)
+}